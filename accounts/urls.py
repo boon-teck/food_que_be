676bca8fa01d3029fa19cbbbbd2006542477b8a5
Boon <boonteck.soh@hotmail.com>
2021-07-16 02:33:13 +0800

    new build, new hope

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from accounts.views import *
 
 urlpatterns = [
-    path('api/login/', login_user_api, name='login_api_page'),
-    path('api/register/', register_user_api, name='register_api_page'),
-    path('api/user/', get_user_api, name='get_user_api_page'),
+    path('login/', login_user_api, name='login_api_page'),
+    path('register/', register_user_api, name='register_api_page'),
+    path('user/', get_user_api, name='get_user_api_page'),
 
 ]
